198d6b4d62ef92916c9b275c3bdcbb0d82c204d4
Andre Freeman <asf@mail.usf.edu>
2020-01-26 19:46:10 -0500

    Fuctions explaination added

--- challenges/functions.js
+++ challenges/functions.js
@@ -36,7 +36,7 @@ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice t
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: Because they both reside in the myFunction function. 
 
 
 const external = "I'm outside the function";
