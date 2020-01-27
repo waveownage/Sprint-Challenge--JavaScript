3165daa9bb3430551e3122fa651277dc511e2b39
Andre Freeman <asf@mail.usf.edu>
2020-01-26 19:56:28 -0500

    created dinosaur objects in objects-arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,11 +7,34 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+tyrannosaurus = {
+  name = `tyrannosaurus`,
+  diet = `carnivorous`,
+  weight = `7000kg`,
+  length = `12m`,
+  period = `Late Cretaceous`;
+  roar: function(){
+    return `RAWERSRARARWERSARARARRRR!`
+  }
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+stegosaurus = {
+  name: `stegosaurus`,
+  diet: `herbivorous`,
+  weight: `2000kg`,
+  length: `9m`,
+  period: `Late Jurassic`
+}
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
+velociraptor = {
+  name: `velociraptor`,
+  diet: `carnivorous`,
+  weight: `15kg`,
+  length: `1.8m`,
+  period: `Late Cretaceous`
+}
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
